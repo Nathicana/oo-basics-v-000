eba88a8d0761ca0cfcc41fc5e07098bf2fd75d4d
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-06 01:47:00 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,8 +9,9 @@ def initialize(brand)
   @brand = brand
 end
 
-def cobble(condition = "new")
+def cobble(condition = "new", repair = "Your shoe is as good as new!"
   @condition = condition
+  @repair = repair
 end
 
 end
